72f2df10837037877f884db0bd93cab82c9d153e
mhselim103 <mhselin103@gmail.com>
2021-08-02 01:28:09 +0600

    loop

---
+++ while-loop-ex.js
@@ -0,0 +1,7 @@
+var number = 0; //loop variable 
+
+while (number < 10) {
+    console.log(number);
+    number++;
+    
+}
---
+++ while-loop.js
@@ -0,0 +1,9 @@
+var roastGiven = 0; //loop variable 
+
+while (roastGiven < 7) {
+    console.log("roast den , please!!!!");
+    console.log(roastGiven);
+    roastGiven++;
+}
+
+
